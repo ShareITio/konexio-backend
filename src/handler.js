4aae8b114aa1b6b773f7f84f9e64965c8f5bfa4c
Hospice <johannhospice.dev@gmail.com>
2020-10-23 22:56:04 +0200

    Fix create sms call

--- src/handler.js
+++ src/handler.js
@@ -9,11 +9,12 @@ module.exports.default = async (event, context) => {
       return makeReturn(await list(2), "200");
     }
     if (event.httpMethod === "POST") {
-      return makeReturn(await create(event.body.body, event.body.to), "200");
+      const { body, to } = JSON.parse(event.body);
+      return makeReturn(await create(to, body), "200");
     }
     return makeReturn(`Unsupported "${event.httpMethod}".`, "400");
   } catch (error) {
     console.error(error);
-    return makeReturn(`An error as occured.`, "400");
+    return makeReturn("An error as occured.", "400");
   }
 };
