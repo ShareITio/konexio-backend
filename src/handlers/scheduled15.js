8044205d1c90d5efbc70ae11af303f8620bb99cb
Hospice <johannhospice.dev@gmail.com>
2020-11-17 18:56:15 +0100

    Start binding leaners with messages on creation

--- src/handlers/scheduled15.js
+++ src/handlers/scheduled15.js
@@ -1,33 +1,64 @@
-const { STATUS_SUCCESS, makeReturn, formattedDate } = require("../tools");
+const { STATUS_SUCCESS, makeReturn } = require("../tools");
 const { list } = require("../twilio");
-const { makeCreate, makeSchema } = require("../airtable");
+const { makeFetcher, makeCreate, makeSchema } = require("../airtable");
 
 const MINUTES = 15;
 
 const schemaMessage = {
-  from: "Téléphone",
-  body: "Message",
-  dateSent: "Date d'envoie",
+  to: "Numéro Konexio de réception",
+  from: "Numéro d'envoi",
+  body: "Contenu du message",
+  dateSent: "Date et heure de réception",
+  status: "Statut du message",
+  learners: "Apprenant lié au numéro",
 };
 const createMessage = makeCreate(
-  "SMS",
+  "Messages",
   makeSchema(schemaMessage, (name) =>
-    schemaMessage.dateSent === name ? formattedDate : (data) => data
+    schemaMessage.dateSent === name
+      ? (data) => data.toISOString()
+      : (data) => data
   )
+);
+
+const schemaLearner = {
+  phone: "Téléphone",
+};
+const fetchLearners = makeFetcher(
+  "Candidatures DigitAll et DigiStart",
+  "Toutes les candidatures",
+  makeSchema(schemaLearner, (name) => (data) => data.get(name))
 );
 
 module.exports.handler = async (event) => {
   console.log(event);
 
   try {
-    const receive = await list({
-      dateSentAfter: new Date(Date.now() - 1000 * (60 * MINUTES)),
-    });
+    const [learners, receive] = await Promise.all([
+      fetchLearners(),
+      list({
+        limit: 3,
+        // dateSentAfter: new Date(Date.now() - 1000 * (60 * MINUTES)),
+      }),
+    ]);
 
     await Promise.all(
       receive.map(({ from, body, dateSent }) => {
         console.log("Adding to airtable MESSAGE", from, body);
-        return createMessage({ from, body, dateSent });
+        console.log(
+          learners.filter(({ phone }) => phone === from).map(({ id }) => id)
+        );
+
+        return createMessage({
+          from,
+          body,
+          // dateSent,
+          status: "À traiter",
+          to: process.env.TWILIO_PHONE,
+          learners: learners
+            .filter(({ phone }) => phone === from)
+            .map(({ id }) => id),
+        });
       })
     );
 
