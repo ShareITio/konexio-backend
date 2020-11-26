24b06eb2917123567a8224d824adeb14b03a7126
Hospice <johannhospice.dev@gmail.com>
2020-11-26 18:41:14 +0100

    Adding crossknowledge handling lambda function

--- src/crossknowledge.js
+++ src/crossknowledge.js
@@ -1,15 +1,17 @@
 const { request } = require("./tools");
 
+const HEADERS = {
+  Host: process.env.CROSSKNOWLEDGE_HOST,
+  "API-KEY": process.env.CROSSKNOWLEDGE_API_KEY,
+  "Cache-Control": "no-cache",
+};
+
 module.exports.getTrainings = () =>
   request({
     hostname: process.env.CROSSKNOWLEDGE_HOST,
     path: "/API/ADMIN/v1/REST/Training/",
     method: "GET",
-    headers: {
-      Host: process.env.CROSSKNOWLEDGE_HOST,
-      "API-KEY": process.env.CROSSKNOWLEDGE_API_KEY,
-      "Cache-Control": "no-cache",
-    },
+    headers: HEADERS,
   });
 
 module.exports.getTrainingSession = (guid) =>
@@ -17,9 +19,40 @@ module.exports.getTrainingSession = (guid) =>
     hostname: process.env.CROSSKNOWLEDGE_HOST,
     path: `/API/ADMIN/v1/REST/Training/${guid}/Session/`,
     method: "GET",
-    headers: {
-      Host: process.env.CROSSKNOWLEDGE_HOST,
-      "API-KEY": process.env.CROSSKNOWLEDGE_API_KEY,
-      "Cache-Control": "no-cache",
-    },
+    headers: HEADERS,
+  });
+
+module.exports.createLearner = (
+  login,
+  {
+    name,
+    firstName,
+    email,
+    entityGuid,
+    language,
+    referenceNumber,
+    customFields,
+    status,
+    start,
+    end,
+  }
+) =>
+  request({
+    hostname: process.env.CROSSKNOWLEDGE_HOST,
+    path: `/API/ADMIN/v1/REST/Learner/`,
+    method: "POST",
+    headers: HEADERS,
+    body: JSON.stringify({
+      login,
+      name,
+      firstName,
+      email,
+      entityGuid,
+      language,
+      referenceNumber,
+      customFields,
+      status,
+      start,
+      end,
+    }),
   });
--- src/handlers/scheduled15.js
+++ src/handlers/scheduled15.js
@@ -1,9 +1,6 @@
-const AWS = require("aws-sdk");
+const { notifyError } = require("../awsServices");
 const { list } = require("../twilio");
 const { createMessage, fetchCandidates } = require("../airtableServices");
-
-AWS.config.update({ region: "eu-west-3" });
-const SNS = new AWS.SNS();
 
 const MINUTES = 15;
 
@@ -52,33 +49,6 @@ module.exports.handler = async (event, context) => {
     return `Record created/received : ${messages.length}`;
   } catch (err) {
     console.error(err);
-
-    const date = new Date(event.time);
-
-    const datestring = `${[
-      date.getDate(),
-      date.getMonth() + 1,
-      date.getFullYear(),
-    ].join("/")} à ${[date.getHours(), date.getMinutes()].join(":")}`;
-
-    const pub = await SNS.publish({
-      Message:
-        `Bonjour,\n\n` +
-        `Vous recevez ce message car une erreur s'est produite sur une fonction hebergée sur AWS.\n\n\n` +
-        `Nom de la fonction : "${context.functionName}"\n\n` +
-        `Date et heure de l'erreur : "${datestring}"\n\n` +
-        `Message de l'erreur : "${err}"\n\n` +
-        `Lien du log de la fonction : https://eu-west-3.console.aws.amazon.com/lambda/home?region=eu-west-3#/functions/scheduled15?tab=monitor\n\n` +
-        `Informations complémentaires: ${JSON.stringify(
-          {
-            event,
-            context,
-          },
-          null,
-          2
-        )}\n`,
-      TopicArn: process.env.ERROR_TOPIC_ARN,
-    }).promise();
-    console.log(pub);
+    return notifyError(err, event, context);
   }
 };
